68d3698ecb19f00694e4d08f6c9da25c3dbbbf7a
Matthew Sevey <mjsevey@gmail.com>
2018-04-13 14:52:14 -0400

    added POST API request for adding blocks to the chain

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 // this allows a user to define a HTTP_PORT at the command license
@@ -9,10 +10,21 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain;
 
+// app middleware
+app.use(bodyParser.json());
+
 // API get request
 // req = request, res = response, provided by express
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
+});
+
+app.post('/mine', (req, res) => {
+  // express automatically adds the body object to req
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+
+  res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
