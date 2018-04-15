25ba17c9f078a9df934f38e7d97bc2140652a844
Matthew Sevey <mjsevey@gmail.com>
2018-04-15 10:25:07 -0400

    created miner class in miner.js, added validTransaction function to transactionPool

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionPool {
   constructor() {
     this.transactions = [];
@@ -15,6 +17,25 @@ class TransactionPool {
 
   existingTransaction(address) {
     return this.transactions.find(t => t.input.address === address);
+  }
+
+  validTransactions() {
+    return this.transactions.filter(t => {
+      const outputTotal = t.outputs.reduce({total, output} => {
+        return total + output.amount;
+      }, 0);
+      if (t.input.amount !== outputTotal) {
+        console.log(`Invalid transaction from ${t.input.address}.`);
+        return;
+      }
+
+      if (!Transaction.verifyTransaction(t)) {
+        console.log(`Invalid signature from ${t.input.address}.`);
+        return;
+      }
+
+      return t;
+    });
   }
 
 }
