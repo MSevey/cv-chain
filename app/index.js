c1a144daad955ddc305132b6aeff3c273b72386d
Matthew Sevey <mjsevey@gmail.com>
2018-04-15 09:53:41 -0400

    added broadcast transactions to p2p server and message types

--- app/index.js
+++ app/index.js
@@ -14,7 +14,7 @@ const app = express();
 const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
-const p2pServer = new P2pServer(bc);
+const p2pServer = new P2pServer(bc, tp);
 
 // app middleware
 app.use(bodyParser.json());
